3db65bc2c814b335f8101d0f0a048429e8145095
Cristian Gallego <cristiangallego40@gmail.com>
2024-03-22 11:21:19 -0500

    primer commit de django diappo 235

---
+++ movies/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
+
+# Create your views here.
+def index(request):
+    movies = Movie.objects.all()
+    
+    return render(request, 'movies/index.html', {'movies': movies})
+
+def detail(request, movie_id):
+    return HttpResponse(movie_id)
